279ee72ef857766caeeca2a1afc315b0cfa8f431
Hao Wu <wuhaoecho@gmail.com>
2024-04-25 18:10:59 +0800

    update

--- admin_backend/settings.py
+++ admin_backend/settings.py
@@ -171,7 +171,12 @@ STATIC_URL = "/static/"
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 WHITENOISE_INDEX_FILE = True
 
 # Default primary key field type
